dbbee4a17aa7f9448dbee73ca64ac3192cda683a
Micah Shute <micah.shute@gmail.com>
2018-07-05 07:16:43 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,10 @@
 class BankAccount
 
   attr_accessor :name
+  attr_reader: balance
   
-  def initialize(name)
+  def initialize(name, balance=1000)
     @name = name
+    @balance = balance
   end
 end
