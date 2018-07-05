1567dbd8fe6263b9098af211175dfbbb912ee45f
Micah Shute <micah.shute@gmail.com>
2018-07-05 07:51:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,4 +25,12 @@ class Transfer
     end
   end
 
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "cancelled"
+    end
+  end
+
 end
