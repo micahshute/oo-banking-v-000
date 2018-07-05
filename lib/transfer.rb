7dac9e0b70b47bbd807f4f89ebe1eb2a753a218c
Micah Shute <micah.shute@gmail.com>
2018-07-05 07:48:47 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,7 @@ class Transfer
       receiver.balance += @amount
       self.status = "complete"
     else
+      self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
